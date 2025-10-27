40065352cedc81556e6afbce4d7139be3fefbd9c
Shane Velez <shanevelez@gmail.com>
2025-10-27 22:24:01 +0000

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,18 @@ function Navbar() {
   return (
     <header className="w-full bg-white shadow-sm">
       <nav className="max-w-5xl mx-auto flex items-center justify-between py-4 px-6">
+        {/* Left: title */}
         <h1 className="font-heading text-2xl text-primary">Cornerstones</h1>
+
+        {/* Right: nav links */}
         <div className="space-x-6 font-sans text-lg">
           <a href="#booking" className="hover:text-primary">Book</a>
-        </div>
-        <div className="space-x-6 font-sans text-lg">
           <a href="/recommend" className="hover:text-primary">Local Recs</a>
+          
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
